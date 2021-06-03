1b70548fcbbfa1614279bba75ba52c2ebce42498
skat9234 <79435289+skat9234@users.noreply.github.com>
2021-06-03 20:56:07 +0530

    Update ProductFeed.js

--- src/pages/ProductFeed.js
+++ src/pages/ProductFeed.js
@@ -4,7 +4,7 @@ import Product from "./Product"
 function ProductFeed({products}) {
     return (
         <div className="bg-gray-100 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
-            {products.map(({ id, title, description, category, image, price}) => (
+            {products?.map(({ id, title, description, category, image, price}) => (
                 <Product 
                   key={id}
                   id={id}
